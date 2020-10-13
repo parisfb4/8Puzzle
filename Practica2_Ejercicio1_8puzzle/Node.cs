9ae6e78e90edcd8f4e58ae4e0a16ed9598e7c984
@OmarAlvarez06 <omaralvarez_cucei@outlook.com>
2020-10-13 11:51:29 -0700

    Update 32:00

--- Practica2_Ejercicio1_8puzzle/Node.cs
+++ Practica2_Ejercicio1_8puzzle/Node.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Practica2_Ejercicio1_8puzzle
 {
@@ -13,11 +16,13 @@ namespace Practica2_Ejercicio1_8puzzle
         private Node parent;                                    //Nodo padre 
         private int[] puzzle = new int[9];                      //Arreglo del puzzle, o estructura actual
         private int x = 0;                                      //Indicador de posicion del 0
+        private int col = 3;                                    //Posiciones del puzzle que es 3 x 3
 
         public int[] Puzzle { get => puzzle; set => puzzle = value; }
         public int X { get => x; set => x = value; }
         internal List<Node> Children { get => children; set => children = value; }
         internal Node Parent { get => parent; set => parent = value; }
+        public int Col { get => col; set => col = value; }
         #endregion
 
         #region Constructor
@@ -36,6 +41,118 @@ namespace Practica2_Ejercicio1_8puzzle
             }
         }
 
-        //
+        //Funcion para verificar que la meta no ha llegado al final o si
+        public bool GoalTest()
+        {
+            bool isGoal = true;
+            int m = puzzle[0];
+
+            for(int i = 1; i < puzzle.Length; i++)
+            {
+                if (m > puzzle[i])
+                    isGoal = false;
+                m = puzzle[i];
+            }
+            return isGoal;
+        }
+
+        //Funcion para copiar el puzzle cuando se realzia un movimiento y poder evaluar los hijos en ese estado. 
+        public void CopyPuzzle(int[] PrimerPuzzle, int[] SecondPuzzle)
+        {
+            for(int i = 0; i < SecondPuzzle.Length; i++)
+            {
+                PrimerPuzzle[i] = SecondPuzzle[i]; 
+            }
+        }
+
+        //Funcion para imprimir el puzzle 
+        public void PrintPuzzle()
+        {
+            Console.WriteLine();
+            int m = 0;
+            for(int i = 0; i < col; i++)
+            {
+                for(int j = 0; j < col; j++)
+                {
+                    Console.WriteLine(puzzle[m] + " ");
+                    m++;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        #region Movimientos Derecha, Izquierda, Abajo y Arriba
+        //Mover a la derecha y obtener hijos posibles
+        public void MoveToRigth(int[] value, int index) //Recibe el puzle actual y el index del 0 o espacio en blanco
+        {
+            if( index % col < Col - 1)
+            {
+                int[] puzzle_auxiliar = new int[9];
+                CopyPuzzle(puzzle_auxiliar, value);
+
+                int temporal = puzzle_auxiliar[index + 1];
+                puzzle_auxiliar[index + 1] = puzzle_auxiliar[index];
+                puzzle_auxiliar[index] = temporal;
+
+                Node child = new Node(puzzle_auxiliar);
+                children.Add(child);
+                child.parent = this; 
+            }
+        }
+
+        //Mover a la Izquierda y obtener hijos posibles
+        public void MoveToLEft(int[] value, int index) //Recibe el puzle actual y el index del 0 o espacio en blanco
+        {
+            if(index % col > 0)
+            {
+                int[] puzzle_auxiliar = new int[9];
+                CopyPuzzle(puzzle_auxiliar, value);
+
+                int temporal = puzzle_auxiliar[index - 1];
+                puzzle_auxiliar[index - 1] = puzzle_auxiliar[index];
+                puzzle_auxiliar[index] = temporal;
+
+                Node child = new Node(puzzle_auxiliar);
+                children.Add(child);
+                child.parent = this;
+            }
+        }
+
+        //Mover Arriba y obtener hijos posibles
+        public void MoveToUp(int[] value, int index)//Recibe el puzle actual y el index del 0 o espacio en blanco
+        {
+            if(index - col >= 0)
+            {
+                int[] puzzle_auxiliar = new int[9];
+                CopyPuzzle(puzzle_auxiliar, value);
+
+                int temporal = puzzle_auxiliar[index - 3];
+                puzzle_auxiliar[index - 3] = puzzle_auxiliar[index];
+                puzzle_auxiliar[index] = temporal;
+
+                Node child = new Node(puzzle_auxiliar);
+                children.Add(child);
+                child.parent = this; 
+            }
+        }
+
+        //Mover Abajo y obtener hijos posibles
+        public void ModeToDown(int[] value, int index) //Recibe el puzle actual y el index del 0 o espacio en blanco
+        {
+            if (index + col < puzzle.Length)
+            {
+                int[] puzzle_auxiliar = new int[9];
+                CopyPuzzle(puzzle_auxiliar, value);
+
+                int temporal = puzzle_auxiliar[index + 3];
+                puzzle_auxiliar[index] = puzzle_auxiliar[index + 3];
+                puzzle_auxiliar[index] = temporal;
+
+                Node child = new Node(puzzle_auxiliar);
+                children.Add(child);
+                child.parent = this;
+            }
+        }
+        #endregion
     }
 }
